10cc1dafef29b5fe5bd441c9991760ed65f71d4b
Jacky <newbdez33@gmail.com>
2018-09-05 18:47:33 +0900

    init

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './styles.scss';
+
+const App = () => {
+  return (
+    <div>
+      <p>@newbdez33</p>
+    </div>
+  );
+}
+
+render(<App />, document.getElementById('app'));
